cb83365e3f58d6c007509f0f9780d8305b72b894
xoreo <mattnappo@gmail.com>
2019-11-29 22:21:38 -0500

    Project all set up.

--- react.md
+++ react.md
@@ -3,6 +3,11 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
 
+## Notes
+### Files
+`index.js` - main javascript file that renders the App component.<br>
+Every component gets a `Component.js`, `Component.css`, and `Component.test.js`<br>
+
 ## Table of Contents
 
 - [Updating to New Releases](#updating-to-new-releases)
--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-// Modules to control application life and create native browser window
-const {
-    app,
-    BrowserWindow
-} = require('electron')
-const path = require('path')
+// Imports
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// The window itself
+let mainWindow;
 
+// Create the main window
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
@@ -17,40 +14,29 @@ function createWindow() {
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
+    });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000')
+    // Load the react app into the window
+    mainWindow.loadURL('http://localhost:3000');
 
-    // Open the DevTools.
+    // Open the DevTools
     // mainWindow.webContents.openDevTools()
 
-    // Emitted when the window is closed.
+    // Kill the window when it is closed
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
-    })
+    });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Create the window when ready
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// Quit the app when all windows are closed
 app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') app.quit()
 })
 
+// Re-create the window when clicked on the dock (macos)
 app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
