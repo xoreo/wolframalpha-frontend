69d105f643a02303825a327ef6029045b2bfb515
xoreo <mattnappo@gmail.com>
2019-12-02 16:18:56 -0500

    Cleaned up the input component.

--- src/Input.js
+++ src/Input.js
@@ -1,91 +1,84 @@
 import React, { Component } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField';
+import { TextField, Button, Container } from '@material-ui/core';
+import { ToastContainer, toast } from 'react-toastify';
 
 import net from './net/net'
-
 import './Input.css';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: 200,
-        },
-    },
-}));
 
 // Input is the component containing the search bar.
 export default class Input extends Component {
+    errorAlert = message => toast.error(message); // Error reporting
+
     constructor(props) {
         super(props);
 
+        // Initialize the state values
         this.state = {
             search: '',
             response: '',
             responseJSON: {}
         };
 
+        // Declare the methods
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         
     }
+    
+    // handleChange is executed when the textbox is changed.
     handleChange(event) {
+        // Update the state to the text in the textbox
         this.setState({search: event.target.value});
     }
 
+    // handleSubmit calls the API with the search input and
+    // updates the state accordingly.
     handleSubmit(event) {
-        // alert('hi');
-        console.log("SUBMIT HANDLED -------");
-
+        // Call the API
         fetch(
-            net.endpoint('search'),
+            net.endpoint('search'), // Get the endpoint
             {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'text/plain'
+                    'Content-Type': 'text/plain' // For CORS reasons
                 },
                 body: JSON.stringify({
                     search_text: this.state.search
                 })
             }
-        ).then(res => res.json())
+        ).then(res => res.json()) // Handle the Promise
         .then(res => {
           if (res.error) {
             // Check no error
-            this.errorAlert(res.error); // Show error
-
-            return; // Return
+            this.errorAlert(res.error);
+            return;
           }
 
+          // Update the state with the response
           this.setState({
             response: res.search_result,
             responseJSON: JSON.parse(res.search_result)
-          }); // Set state
-
-          console.log(res);
+          });
 
         });
 
-        event.preventDefault();
+        event.preventDefault(); // Prevent the page from reloading
     }
-    // const classes = useStyles();
-    
     
     render() {
         return (
-
-        <Container>
-            <p>hi world</p>
-            <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
-                <TextField label="Search box here" variant="outlined" onChange={this.handleChange} value={this.state.search} />
-                <Button variant="outlined" color="primary" type="submit">Search</Button>
-                <TextField label="TEST RESPONSE" variant="outlined" value={this.state.response} />
-            </form>
-        </Container>
+            <Container>
+                <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+                    <TextField label="Search box here" variant="outlined"
+                        onChange={this.handleChange} value={this.state.search} />
+                    <Button variant="outlined" color="primary"
+                        type="submit">Search</Button>
+                    <TextField label="TEST RESPONSE" variant="outlined"
+                        value={this.state.response} />
+                </form>
+                <ToastContainer />
+            </Container>
         )
     };
 }
