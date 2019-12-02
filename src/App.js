01473cc26b871e62f6005a78b28a22ec046e8d69
xoreo <mattnappo@gmail.com>
2019-12-02 16:28:06 -0500

    Cleaned up app.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 
-import Header from './Header'
-import History from './History'
-import MainArea from './MainArea'
-import Test from './Test'
+import Header from './Header';
+import History from './History';
+import MainArea from './MainArea';
+
 import './App.css';
 
 // App is the main component of the entire app.
@@ -13,7 +13,6 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Test />
                 <Header />
                 <Grid container spacing={2}>
                     <Grid item xs>
